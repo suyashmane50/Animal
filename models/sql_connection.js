84e720009aa9ffc932c998ed8b8465ced06945fd
Suyash Mane <suyashmane584@gmail.com>
2025-11-07 11:53:22 +0530

    deploying on render

--- models/sql_connection.js
+++ models/sql_connection.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "Suyash@123",
+  database: process.env.DB_NAME || "animal",
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
